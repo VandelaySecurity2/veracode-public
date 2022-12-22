6c64e766a871ebd8c49dd53a5b564d76cd621ffd
tali-legit <87929712+tali-legit@users.noreply.github.com>
2022-12-22 14:17:09 +0200

    Update Main.java

--- app/src/main/java/com/veracode/Main.java
+++ app/src/main/java/com/veracode/Main.java
@@ -1,11 +1,12 @@
-package veracode;
+package com.veracode;
 
 public class Main {
   
     public static void main(String[] args) {
-      private String name = 'my name';
-      private String password = '12345';
-      
+      String name = "my name";
+      String password = "12345";
+
+        System.out.println("My name is" + name);
       System.out.println("My password is" + password);
     }
 }
