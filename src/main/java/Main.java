df6490d6cf6720e443da9dc911c1e958ffbf3f25
tali-legit <87929712+tali-legit@users.noreply.github.com>
2022-12-22 10:27:05 +0200

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+  
+    public static void main(String[] args) {
+      private String name = 'my name';
+      private String password = '12345';
+      
+      System.out.println("My password is" + password);
+    }
+}
